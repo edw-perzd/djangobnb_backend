3549ba6ba309afac1e35f1ccf8d0300fa75d5703
Eduardo <legna246.dav@gmail.com>
2025-03-12 00:39:01 -0600

    05 Create app for Properties and Model and serializer for properties

--- djangobnb_backend/property/models.py
+++ djangobnb_backend/property/models.py
@@ -9,7 +9,7 @@ class Property(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.IntegerField()
+    price_per_night = models.IntegerField()
     bedrooms = models.IntegerField()
     bathrooms = models.IntegerField()
     guests = models.IntegerField()
