812fbf274dd6b862686a6eb059ef8a6b3c22ec40
Eduardo <legna246.dav@gmail.com>
2025-03-18 01:43:45 -0600

    20 Start new conversations

--- djangobnb_backend/chat/urls.py
+++ djangobnb_backend/chat/urls.py
@@ -4,5 +4,6 @@ from . import api
 
 urlpatterns = [
     path('', api.conversations_list, name='api_conversations_list'),
+    path('start/<uuid:user_id>/', api.conversations_store, name='api_conversations_store'),
     path('<uuid:pk>/', api.conversations_detail, name='api_conversations_detail')
 ]
